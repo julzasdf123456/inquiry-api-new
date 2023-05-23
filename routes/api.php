579e2715646f130624ad5642f07e549ecec062c0
Julio Lopez <julzasdf123456@gmail.com>
2023-05-23 13:28:46 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -22,38 +22,38 @@ use App\Http\Controllers\API\ThirdPartyAPI;
 */
 
 // LOGIN AND REGISTER
-Route::post('login', [UserController::class, 'login']);
-Route::post('register', [UserController::class, 'register']);
-Route::get('logout', [UserController::class, 'logout']);
-Route::post('reset-password', [UserController::class, 'resetPassword']);
+// Route::post('login', [UserController::class, 'login']);
+// Route::post('register', [UserController::class, 'register']);
+// Route::get('logout', [UserController::class, 'logout']);
+// Route::post('reset-password', [UserController::class, 'resetPassword']);
 
 // ACCOUNT MASTER
 Route::get('get-account-by-account-number', [AccountMastersController::class, 'getAccountByAccountNumber']);
-Route::post('update-contact-info', [AccountMastersController::class, 'updateContactInfo']);
+// Route::post('update-contact-info', [AccountMastersController::class, 'updateContactInfo']);
 
 // ACCOUNT LINKS
-Route::get('get-linked-accounts', [AccountLinksController::class, 'getLinkedAccounts']);
-Route::post('link-account', [AccountLinksController::class, 'linkAccount']);
-Route::get('remove-link', [AccountLinksController::class, 'removeLink']);
-Route::get('get-pending-accounts', [AccountLinksController::class, 'getPendingAccounts']);
+// Route::get('get-linked-accounts', [AccountLinksController::class, 'getLinkedAccounts']);
+// Route::post('link-account', [AccountLinksController::class, 'linkAccount']);
+// Route::get('remove-link', [AccountLinksController::class, 'removeLink']);
+// Route::get('get-pending-accounts', [AccountLinksController::class, 'getPendingAccounts']);
 
 // TOKEN
-Route::post('insert-token', [UserController::class, 'insertToken']);
+// Route::post('insert-token', [UserController::class, 'insertToken']);
 
 // BILLS
 Route::get('get-latest-bills', [BillsController::class, 'getLatestBills']);
-Route::get('get-unpaid-bills', [BillsController::class, 'getUnpaidBills']);
+// Route::get('get-unpaid-bills', [BillsController::class, 'getUnpaidBills']);
 Route::get('get-bill-details', [BillsController::class, 'getBillDetails']);
-Route::get('get-account-information', [BillsController::class, 'getAccountInformation']);
-Route::get('get-previous-for-graph', [BillsController::class, 'getPreviousForGraph']);
-Route::get('get-all-bill-by-year', [BillsController::class, 'getAllBillByYear']);
+// Route::get('get-account-information', [BillsController::class, 'getAccountInformation']);
+// Route::get('get-previous-for-graph', [BillsController::class, 'getPreviousForGraph']);
+// Route::get('get-all-bill-by-year', [BillsController::class, 'getAllBillByYear']);
 
 // Notifications
-Route::get('get-notifications', [NotifiersController::class, 'getNotifications']);
+// Route::get('get-notifications', [NotifiersController::class, 'getNotifications']);
 
 // VERIFICATION
-Route::get('send-sms-test', [VerificationController::class, 'sendSmsTest']);
-Route::get('get-email-from-user', [VerificationController::class, 'getEmailFromUser']);
+// Route::get('send-sms-test', [VerificationController::class, 'sendSmsTest']);
+// Route::get('get-email-from-user', [VerificationController::class, 'getEmailFromUser']);
 
 
 Route::group(['middleware' => 'auth:api'], function(){
@@ -63,5 +63,5 @@ Route::group(['middleware' => 'auth:api'], function(){
 /**
  * THIRD PARTY API
  */
-Route::get('get-bill-by-account-and-period', [ThirdPartyAPI::class, 'getBillsByAccountAndPeriod']);
-Route::post('transact', [ThirdPartyAPI::class, 'transact']);
+// Route::get('get-bill-by-account-and-period', [ThirdPartyAPI::class, 'getBillsByAccountAndPeriod']);
+// Route::post('transact', [ThirdPartyAPI::class, 'transact']);
